1eedf64f023d207e27e90a389db8c6fa83b2cea5
komplamoose2 <iamjewel4445@gmail.com>
2022-03-17 15:48:37 +0900

    11650, solved

---
+++ BOJ_Beta_Problems/11650f/11650.cpp
@@ -0,0 +1,46 @@
+// 11650, Silver 5 좌표 정렬하기
+// https://www.acmicpc.net/problem/11650
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct Coordinate{
+    int x;
+    int y;
+};
+
+vector<Coordinate> v;
+
+bool COMP(Coordinate a, Coordinate b)
+{
+    if (a.x == b.x){
+        return a.y < b.y;
+    }
+    return a.x < b.x;
+}
+
+void solve(int n)
+{
+    for (int i=0; i<n ;i++){
+        Coordinate input;
+        cin >> input.x >> input.y;
+        v.push_back(input);
+    }
+
+    sort(v.begin(), v.end(), COMP);
+}
+
+int main(void)
+{
+    int n;
+
+    cin >> n;
+
+    solve(n);
+
+    for (int i=0 ; i<n ;i++){
+        cout << v[i].x << " " << v[i].y << "\n";
+    }
+}
