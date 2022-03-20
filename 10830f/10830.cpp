9145f90a064b06b71a052f3221646ad320547726
komplamoose2 <iamjewel4445@gmail.com>
2022-03-20 22:30:29 +0900

    10830, solved

--- 10830f/10830.cpp
+++ 10830f/10830.cpp
@@ -1,13 +1,14 @@
 // 10830, Gold 4, 행렬 제곱, Not Solved
 // https://www.acmicpc.net/problem/10830
-// TODO: https://www.acmicpc.net/problem/13171 거듭제곱 문제 먼저 풀고 도전하자. 
 #include <iostream>
 
 using namespace std;
+using ll = long long;
+
+const int M = 1000;
 
 long long matrix[5][5] = {0,};
 long long result[5][5] = {0,};
-long long temp[5][5] = {0,};
 
 int n; 
 
@@ -19,19 +20,36 @@ void calc(int n, long long a[][5], long long b[][5], long long c[][5])
             for (int k=0; k<n;k++){
                 sum+=a[i][k]*b[k][j];
             }
-            c[i][j]=sum;
+            c[i][j]=sum%1000;
         }
     }
 }
 
-void solve(int n, long long m)
+void solve(int a, long long b)
 {
+    long long temp[5][5] = {{1,0,0,0,0},{0,1,0,0,0},{0,0,1,0,0},{0,0,0,1,0},{0,0,0,0,1}};
+    while(b)
+    {
+        if (b&1){ // 비트연산 b가 짝수면 False, 홀수면 True
+            calc(a,temp,matrix,result);
+            for (int i=0 ; i<a ; i++){
+                for (int j=0 ; j<a ; j++){
+                    temp[i][j]=result[i][j];
+                }
+            }
+        }
+        calc(n,matrix,matrix,result);
+        for (int i=0 ; i<a ; i++){
+                for (int j=0 ; j<a ; j++){
+                    matrix[i][j]=result[i][j];
+                }
+            }
+        b >>= 1; //오른쪽으로 비트 한칸 밀기, 2로 나누는 것과 같음.
+    }
 
-    
-
-    for (int i=0; i<n; i++){
+    for (int i=0; i<n;i++){
         for (int j=0 ; j<n ; j++){
-            cout << result[i][j] % 1000 << " ";
+            cout << temp[i][j] << " ";
         }
         cout << "\n";
     }
@@ -48,7 +66,6 @@ int main()
     for (int i=0 ; i<n ;i++){
         for (int j=0 ; j<n ;j++){
             cin >> matrix[i][j];
-            temp[i][j]=matrix[i][j];
         }
     }
 
