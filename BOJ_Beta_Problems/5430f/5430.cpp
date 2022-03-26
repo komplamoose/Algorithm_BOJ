1ee0ec727bc7ef71f296f6735c5ed475bb4d6634
komplamoose2 <iamjewel4445@gmail.com>
2022-03-27 00:52:14 +0900

    5430.solved

--- BOJ_Beta_Problems/5430f/5430.cpp
+++ BOJ_Beta_Problems/5430f/5430.cpp
@@ -13,43 +13,82 @@ void solve(int n)
     for (int i=0; i<n; i++){
         int nums;
         string func, arrs;
-        cin >> func >> nums >> arrs;
-        arrs.erase(remove(arrs.begin(), arrs.end(), '['),arrs.end()); // 양 옆 대괄호 삭제
-        arrs.erase(remove(arrs.begin(), arrs.end(), ']'),arrs.end());
-
-        string delim = ",";
         deque<int> dq;
+        cin >> func >> nums;
+        // cin >> arrs;
+        // arrs.erase(remove(arrs.begin(), arrs.end(), '['),arrs.end()); // 양 옆 대괄호 삭제
+        // arrs.erase(remove(arrs.begin(), arrs.end(), ']'),arrs.end());
 
-        int pos = 0;
-        while((pos=arrs.find(delim))!=string::npos){
-            dq.push_back(stoi(arrs.substr(0,pos)));
-            arrs.erase(0,pos+delim.length());
-        } // 문자열 파싱 후 정수로 변환해서 deque에 삽입 완료
+        // string delim = ",";
+        // int pos = 0;
+        // while((pos=arrs.find(delim))!=string::npos){
+        //     dq.push_back(stoi(arrs.substr(0,pos)));
+        //     arrs.erase(0,pos+delim.length());
+        // } // 문자열 파싱 후 정수로 변환해서 벡터에 삽입 완료
+
+    
+        // if (nums!=0){
+        //     dq.push_back(stoi(arrs.substr(0,pos)));
+        // }
+        char c;
+        int t=nums;
+
+        cin >> c;
+        if (nums==0){
+            cin >> c;
+        }
+        while(t--){
+            int temp;
+            cin >> temp;
+            cin >> c;
+            dq.push_back(temp);
+        }
+
 
         int trg=0;
+        int r=0;
         for (int j=0; j<func.length();j++){
             if (func[j]=='R'){
-                reverse(dq.begin(),dq.end());
+                if (r==0)
+                    r=1;
+                else
+                    r=0;
             }else if (func[j]=='D'){
                 if (dq.size()==0){
                     trg = 1;
                 }else{
-                    dq.pop_front();
+                    if (r==0){
+                        nums--;
+                        dq.pop_front();
+                    }else{
+                        nums--;
+                        dq.pop_back();
+                    }
                 }
             }
         }
         if (trg==1)
             cout << "error\n";
         else{
-            cout << "[";
-            for (int j=0;j<nums;j++){
-                cout << dq.front();
-                if (j!=nums-1){
-                    cout << ",";
+            if (r==0){
+                cout << "[";
+                for (int j=0;j<nums;j++){
+                    cout << dq[j];
+                    if (j!=nums-1){
+                        cout << ",";
+                    }
                 }
-                dq.pop_front();
+                cout << "]\n";
+            }else{
+                cout << "[";
+                for (int j=nums-1;j>=0;j--){
+                    cout << dq[j];
+                    if (j!=0){
+                        cout << ",";
+                    }
+                }
+                cout << "]\n";
             }
-            cout << "]\n";
         }
     }
 }
@@ -57,8 +96,12 @@ void solve(int n)
 int main(void)
 {
     int n;
+    cin.tie(NULL); 
+    ios_base::sync_with_stdio(false);
 
     cin >> n;
+
+    solve(n);
 
     return 0;
 }
