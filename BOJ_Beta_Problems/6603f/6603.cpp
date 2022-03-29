b0ec63e57565e63656f8e7bbc7a73a8c4bbbf3c4
komplamoose2 <iamjewel4445@gmail.com>
2022-03-29 14:33:15 +0900

    6603, solved

--- BOJ_Beta_Problems/6603f/6603.cpp
+++ BOJ_Beta_Problems/6603f/6603.cpp
@@ -3,35 +3,56 @@
 // TODO: 백트래킹 좀 더 공부하고 다시 공부하자. 
 #include <iostream>
 #include <vector>
+#include <cstring>
 
 using namespace std;
 
-void dfs(vector<int>& v)
+vector<int> v;
+int arr[49]={0,};
+int check[49]={0,};
+
+void solve(int k)
 {
-
-}
-
-void solve()
-{
-    int n;
-    while(1)
-    {
-        cin >> n;
-        if (n==0) break;
-
-        vector<int> chosen;
-        for (int i=0 ; i<n; i++)
-        {
-            int input;
-            cin >> input;
-            chosen.push_back(input);
-        }
-    }
+	if (k==6)
+	{
+		for (int i=0; i<k ; i++)
+		{
+			cout << arr[i] <<" ";
+		}
+		cout << "\n";
+	}else{
+		for (int i=0; i<v.size();i++)
+		{
+			if (check[i]==0){
+				if (v[i]<arr[k-1]) continue;
+				arr[k]=v[i];
+				check[i]=1;
+				solve(k+1);
+				check[i]=0;
+			}
+		}
+	}
 }
 
 int main()
 {
-    solve();
-
+	int n;
+	cin >> n;
+	while (n!=0)
+	{
+		for (int i=0 ; i<n ; i++)
+		{
+			int input;
+			cin >> input;
+			v.push_back(input);
+		}
+		solve(0);
+		cout << "\n";
+		v.clear();
+		memset(arr,0,49);
+		memset(check,0,49);
+		cin >> n;
+	}
+	
     return 0;
 }
