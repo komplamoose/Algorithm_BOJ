a20f4d123b1d8f680c5a34932b69d28d6e8b7938
komplamoose2 <iamjewel4445@gmail.com>
2022-04-27 16:33:57 +0900

    11051,0

---
+++ DP/11051f/11051.cpp
@@ -0,0 +1,38 @@
+// 11051, 이항계수 2
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int v[200000];
+int a[2];
+int m;
+
+void bin(int n, int k)
+{
+    for (int i=0 ; i<=n ; i++){
+        for (int j=0; j <=min(i,k); j++){
+            if (j==0 || j == i){
+                v[j]=1;
+                a[0]=v[j];
+            }
+            else{
+                a[1]=a[0];
+                a[0]=v[j];
+                v[j]= (a[1]+v[j])%10007;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+
+    bin(n,k);
+
+
+    cout << v[k] << "\n";
+    return 0;
+}
