1791ca142e882b06d96ada7104e224ff5bb011a5
komplamoose2 <iamjewel4445@gmail.com>
2022-07-12 13:50:20 +0900

    16953, Backtracking

---
+++ DataStructure/Tree/1991.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+typedef struct node* nodePointer;
+struct node{
+    char type;
+    nodePointer leftChild;
+    nodePointer rightChuld;
+};
+
+
+int main()
+{
+    int n;
+
+    
+
+    return 0;
+}
